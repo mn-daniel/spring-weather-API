fae58779210093e862fd063356106e8420916a27
Daniel N <dev.mndaniel@gmail.com>
2023-01-15 19:24:28 +0300

    Initial commit

---
+++ src/main/java/com/hackerrank/weather/controller/WeatherApiRestController.java
@@ -0,0 +1,51 @@
+package com.hackerrank.weather.controller;
+
+import com.hackerrank.weather.model.Weather;
+import com.hackerrank.weather.repository.WeatherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/weather")
+public class WeatherApiRestController {
+
+    @Autowired
+    private WeatherRepository weatherDataRepository;
+
+    @PostMapping
+    public ResponseEntity<Weather> createWeatherData(@Valid @RequestBody Weather weather) {
+        Weather createdWeather = weatherDataRepository.save(weather);
+        return new ResponseEntity<Weather>(createdWeather, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Weather>> getWeatherData() {
+        List<Weather> weatherData = weatherDataRepository.findAll();
+        return new ResponseEntity<List<Weather>>(weatherData, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Weather> getWeatherDataById(@PathVariable(value = "id") Integer id) {
+        Optional<Weather> weatherData = weatherDataRepository.findById(id);
+        if (!weatherData.isPresent()) {
+            return new ResponseEntity<Weather>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Weather>(weatherData.get(), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Weather> deleteWeatherData(@PathVariable(value = "id") Integer id) {
+        Optional<Weather> weatherData = weatherDataRepository.findById(id);
+        if (!weatherData.isPresent()) {
+            return new ResponseEntity<Weather>(HttpStatus.NOT_FOUND);
+        }
+        weatherDataRepository.delete(weatherData.get());
+        return new ResponseEntity<Weather>(HttpStatus.NO_CONTENT);
+    }
+}
